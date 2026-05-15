39f18ec1f40681de82b05559f6b9a608f7dc5fb7
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2026-05-15 08:10:42 -0600

    deploy all

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
-podman compose build --no-cache capture
-podman compose up -d capture
+#!/usr/bin/env bash
+set -e
+git pull
+podman compose build --no-cache
+podman compose up -d --force-recreate
+podman compose ps
