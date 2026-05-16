c28764e40b30437323627d48c9406948bf48a831
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2026-05-16 13:04:30 -0600

    docker/podman stats

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@ set -e
 if [ "$(hostname)" = "valhalla" ]; then
   export APP_PORT=8001
   ENGINE=docker
+  export CONTAINER_SOCK=/var/run/docker.sock
 else
   ENGINE=podman
+  export CONTAINER_SOCK="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}/podman/podman.sock"
 fi
 git pull
 $ENGINE compose build --no-cache capture web
