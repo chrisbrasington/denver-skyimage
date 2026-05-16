52f9566e3b90627d1f4f8ad46bc2255ce94c1398
chrisbrasington <1186896+chrisbrasington@users.noreply.github.com>
2026-05-15 18:08:20 -0600

    dynamic portish

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
+if [ "$(hostname)" = "valhalla" ]; then
+  export APP_PORT=8001
+fi
 git pull
 podman compose build --no-cache capture web
 podman compose up -d --force-recreate capture web
